d8ecdb98a51bf481414a8297d5778b04fd30bbd6
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2019-07-26 09:01:30 -0400

    Add product list screen

---
+++ Cabify CheckoutTests/UI/ProductList/ProductListViewControllerTests.swift
@@ -0,0 +1,25 @@
+//
+//  ProductListViewControllerTests.swift
+//  Cabify CheckoutTests
+//
+//  Created by Ilbert Esculpi on 7/26/19.
+//  Copyright © 2019 Cabify. All rights reserved.
+//
+
+import XCTest
+
+class ProductListViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testDummy() {
+        XCTAssertTrue(1 == 1)
+    }
+
+}
